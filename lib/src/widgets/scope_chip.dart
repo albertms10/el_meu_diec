8e84258888e33b9b7ce136ce52b9f8d73c06b631
Albert Mañosa <albertmasa2@gmail.com>
2023-09-09 22:15:18 +0200

    feat(autocomplete_entry_card): :sparkles: improve overall layout and UX (#14)
    * feat(autocomplete_entry_card): :sparkles: improve overall layout and UX
    
    * perf(definition_entry_sense): :zap: use `const` in `fontFeatures` list

---
+++ lib/src/widgets/scope_chip.dart
@@ -0,0 +1,51 @@
+import 'package:el_meu_diec/model.dart';
+import 'package:flutter/material.dart';
+
+class ScopeChip extends StatelessWidget {
+  final Scope scope;
+  final bool interactive;
+
+  const ScopeChip({super.key, required this.scope, this.interactive = true});
+
+  void _onTap(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(scope.label),
+        duration: const Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(4)),
+      child: Material(
+        color: Colors.grey[100],
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+          side: BorderSide(color: Color(0xFFE0E0E0)),
+        ),
+        child: InkWell(
+          onTap: interactive ? () => _onTap(context) : null,
+          child: Padding(
+            padding: const EdgeInsetsDirectional.only(
+              start: 6,
+              end: 6,
+              top: 3,
+              bottom: 4,
+            ),
+            child: Text(
+              scope.name,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
