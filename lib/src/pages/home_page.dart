7c6e4da6bb5c297aff086289c4f4b508f268fe08
Albert Mañosa <albertmasa2@gmail.com>
2023-10-28 10:15:18 +0200

    feat(home_page): :sparkles: add logo watermark

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:el_meu_diec/model.dart';
 import 'package:el_meu_diec/src/widgets/autocomplete_entry_card.dart';
 import 'package:el_meu_diec/src/widgets/search_bar_results.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 
@@ -23,44 +24,66 @@ class HomePage extends StatelessWidget {
         backgroundColor: backgroundColor,
         surfaceTintColor: backgroundColor,
         shadowColor: backgroundColor,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.bookmark),
-            onPressed: () => (Platform.isAndroid
-                ? showBarModalBottomSheet
-                : showCupertinoModalBottomSheet)<void>(
-              context: context,
-              builder: (context) {
-                final bookmarks =
-                    Provider.of<BookmarkCollection>(context).bookmarks;
-                final wordCache = Provider.of<WordCache>(context);
-
-                return Scaffold(
-                  body: Align(
-                    alignment: Alignment.topCenter,
-                    child: ListView.builder(
-                      itemCount: bookmarks.length,
-                      itemBuilder: (context, index) {
-                        final word = wordCache.wordFromId(
-                          bookmarks.entries.elementAt(index).key,
-                        )!;
-
-                        return AutocompleteEntryCard(
-                          query: word.word,
-                          word: word,
-                        );
-                      },
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+        actions: const [
+          _BookmarksIconButton(),
         ],
       ),
       backgroundColor: backgroundColor,
-      body: const SafeArea(
-        child: SearchBarResults(),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              heightFactor: 4.5,
+              child: SizedBox(
+                height: 140,
+                child: SvgPicture.asset(
+                  'assets/images/el_meu_diec_logo_watermark.svg',
+                ),
+              ),
+            ),
+            const SearchBarResults(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _BookmarksIconButton extends StatelessWidget {
+  const _BookmarksIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.bookmark),
+      tooltip: 'La meva coŀlecció',
+      onPressed: () => (Platform.isAndroid
+          ? showBarModalBottomSheet
+          : showCupertinoModalBottomSheet)<void>(
+        context: context,
+        builder: (context) {
+          final bookmarks = Provider.of<BookmarkCollection>(context).bookmarks;
+          final wordCache = Provider.of<WordCache>(context);
+
+          return Scaffold(
+            body: Align(
+              alignment: Alignment.topCenter,
+              child: ListView.builder(
+                itemCount: bookmarks.length,
+                itemBuilder: (context, index) {
+                  final word = wordCache.wordFromId(
+                    bookmarks.entries.elementAt(index).key,
+                  )!;
+
+                  return AutocompleteEntryCard(
+                    query: word.word,
+                    word: word,
+                  );
+                },
+              ),
+            ),
+          );
+        },
       ),
     );
   }
