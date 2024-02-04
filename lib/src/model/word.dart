f77d758994199bf7e7a419b1db5184a9461ec2ca
Albert Mañosa <albertmasa2@gmail.com>
2024-02-05 00:16:50 +0100

    perf(word): ⚡️ make class immutable

--- lib/src/model/word.dart
+++ lib/src/model/word.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/foundation.dart' show immutable;
+
 import 'definition_entry_sense.dart';
 
+@immutable
 final class Word {
   final String id;
   final String word;
   final List<DefinitionEntrySense>? senses;
 
-  Word({required this.id, required this.word, required this.senses});
+  const Word({required this.id, required this.word, required this.senses});
 }
