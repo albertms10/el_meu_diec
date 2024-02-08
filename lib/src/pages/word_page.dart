3df29a2e3005bf5f66f891cc81b2650a3576766d
Albert Mañosa <albertmasa2@gmail.com>
2024-02-09 00:27:42 +0100

    feat(word_page): ✨ show `AppBar` elevation when scrolled underneath

--- lib/src/pages/word_page.dart
+++ lib/src/pages/word_page.dart
@@ -15,36 +15,46 @@ class WordPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: SelectableText(
-          word.word,
-          maxLines: 1,
-          style: theme.textTheme.displaySmall!.copyWith(fontSize: 32),
+    return ClipRRect(
+      borderRadius: const BorderRadiusDirectional.only(
+        topStart: Radius.circular(30),
+        topEnd: Radius.circular(30),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          scrolledUnderElevation: 6,
+          elevation: 2,
+          title: SelectableText(
+            word.word,
+            maxLines: 1,
+            style: theme.textTheme.displaySmall!.copyWith(
+              fontSize: 32,
+              // See https://github.com/flutter/flutter/issues/80434
+              overflow: TextOverflow.fade,
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          leading: IconButton.filledTonal(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const Icon(Icons.close),
+          ),
+          centerTitle: true,
+          actions: [
+            BookmarkButton(word: word, isTonalFilled: true),
+            IconButton.filledTonal(
+              onPressed: () => Share.shareUri(const DIECRoutes().wordUri(word)),
+              icon: const Icon(Icons.ios_share_rounded),
+            ),
+            const SizedBox(width: 4),
+          ],
+          toolbarHeight: 72,
         ),
         backgroundColor: Colors.transparent,
-        surfaceTintColor: Colors.transparent,
-        shadowColor: Colors.transparent,
-        leading: IconButton.filledTonal(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(Icons.close),
-        ),
-        centerTitle: true,
-        actions: [
-          BookmarkButton(word: word, isTonalFilled: true),
-          IconButton.filledTonal(
-            onPressed: () => Share.shareUri(const DIECRoutes().wordUri(word)),
-            icon: const Icon(Icons.ios_share_rounded),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsetsDirectional.all(20),
+            child: DefinitionEntrySenses(word: word),
           ),
-          const SizedBox(width: 4),
-        ],
-        toolbarHeight: 72,
-      ),
-      backgroundColor: Colors.transparent,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsetsDirectional.all(20),
-          child: DefinitionEntrySenses(word: word),
         ),
       ),
     );
