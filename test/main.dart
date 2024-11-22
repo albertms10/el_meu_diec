8247b8cff759e273e11d5be8c2ec54c4dc454901
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-11-23 00:59:29 +0100

    test(definition_entry_senses): 🧪 add test cases for `fetch` (#97)
    * test(definition_entry_senses): 🧪 add test cases for `fetch`
    
    Signed-off-by: Albert Mañosa <albertmasa2@gmail.com>
    
    * refactor(repositories): ♻️ add entrypoint export file
    
    Signed-off-by: Albert Mañosa <albertmasa2@gmail.com>
    
    ---------
    
    Signed-off-by: Albert Mañosa <albertmasa2@gmail.com>

--- test/main.dart
+++ test/main.dart
@@ -1,10 +1,13 @@
 import 'model/definition_entry_sense_test.dart' as definition_entry_sense_test;
+import 'model/definition_entry_senses_test.dart'
+    as definition_entry_senses_test;
 import 'model/gender_test.dart' as gender_test;
 import 'model/scope_test.dart' as scope_test;
 import 'model/search_condition_test.dart' as search_condition_test;
 
 void main() {
   definition_entry_sense_test.main();
+  definition_entry_senses_test.main();
   gender_test.main();
   scope_test.main();
   search_condition_test.main();
